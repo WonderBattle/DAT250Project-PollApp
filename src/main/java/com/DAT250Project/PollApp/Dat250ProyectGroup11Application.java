60d8f8f9a9284113e27de5b3a2f2d2a3e3e8a297
WonderBattle <elenacanchog@gmail.com>
2025-10-21 12:12:42 +0200

    Initial commit

---
+++ src/main/java/com/DAT250Project/PollApp/Dat250ProyectGroup11Application.java
@@ -0,0 +1,13 @@
+package com.DAT250Project.PollApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dat250ProyectGroup11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dat250ProyectGroup11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/DAT250Project/PollApp/Dat250ProyectGroup11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DAT250Project.PollApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dat250ProyectGroup11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
