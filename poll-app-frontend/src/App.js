a3a78a4c67ec842b91e33af24a05dbaddd178bb0
Beke Eniko Andrea <enikoandrea12@gmail.com>
2025-11-13 18:38:24 +0100

    add button to navigate to public dashboard

--- poll-app-frontend/src/App.js
+++ poll-app-frontend/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Dashboard from "./pages/DashboardPage";
 import VotingPage from "./pages/VotingPage";
+import PublicDashboardPage from "./pages/PublicDashboardPage";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/dashboard" element={<Dashboard/>} />
             <Route path="/vote/:pollId" element={<VotingPage />} />
+            <Route path="/publicdashboard" element={<PublicDashboardPage />} />
         </Routes>
     );
 }
--- poll-app-frontend/src/pages/HomePage.jsx
+++ poll-app-frontend/src/pages/HomePage.jsx
@@ -4,13 +4,23 @@ import "../styles/HomePage.css"
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import RegisterForm from "../components/RegisterForm";
+import {useNavigate} from "react-router-dom";
 
 const HomePage = () => {
     const [showRegister, setShowregister] = useState(false);
+    const navigate = useNavigate()
     return (
         <div className="desktop-1">
             <Header/>
             <main className="main-content">
+                <div className="public-polls-container">
+                    <button
+                        className="public-polls-btn"
+                        onClick={() => navigate("/publicdashboard")}
+                    >
+                        Public Polls
+                    </button>
+                </div>
                 {showRegister ? (
                     <RegisterForm onSwitch={() => setShowregister(false)} />
                 ) : (
